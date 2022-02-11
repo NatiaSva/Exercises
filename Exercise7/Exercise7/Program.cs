a01091d5d79214c56fb55319f883b4260a8a992b
NatiaSva <55664887+NatiaSva@users.noreply.github.com>
2022-02-11 19:13:23 +0200

    1

---
+++ Exercise7/Exercise7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueLinkedList<int> q = new QueueLinkedList<int>();
+
+            q.Enqueue(5);
+            q.Enqueue(7);
+            q.Enqueue(6);
+            q.Enqueue(3);
+            Console.WriteLine(q.ToString());
+            q.Dequeue();
+            Console.WriteLine(q.ToString());
+        }
+    }
+}
---
+++ Exercise7/Exercise7/QueueLinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise7
+{
+    public class QueueLinkedList<T>
+    {
+        private Node<T> front;
+        private Node<T> rear;
+        private int count;
+
+
+        public void Enqueue(T x)
+        {
+            Node<T> newNode = new Node<T>(x);
+            if (front == null)
+            {
+                front = newNode;
+                rear = front;
+            }
+            else
+            {
+                rear.Next = newNode;
+                rear = rear.Next;
+            }
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (front == null)
+            {
+                throw new Exception("Queue is empty");
+            }
+
+            T result = front.Value;
+            front = front.Next;
+            count--;
+            return result;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append("{");
+            Node<T> node = front;
+            for (int i = 0; i < count - 1; i++)
+            {
+                stringBuilder.Append(node.Value + ",");
+                node = node.Next;
+            }
+            if (count > 0)
+                stringBuilder.Append(node.Value);
+            stringBuilder.Append("}");
+            return stringBuilder.ToString();
+        }
+
+    }
+}
